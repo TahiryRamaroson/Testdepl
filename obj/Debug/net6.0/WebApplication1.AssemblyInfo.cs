3227b836f590a2738af465303290a02480a86f85
TahiryHenitsoa <118928190+TahiryRamaroson@users.noreply.github.com>
2024-07-22 17:10:54 +0300

    test2

--- obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Testdepl")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Testdepl")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Testdepl")]
+[assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
